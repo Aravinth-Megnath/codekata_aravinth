1576c209a3a6d1618ad91844a7a5d22b8fb35a0e
Aravinth Meganathan <120720408+Aravinth-Megnath@users.noreply.github.com>
2024-12-17 08:27:43 +0530

    Update basics6.py

--- basics6.py
+++ basics6.py
@@ -1,18 +1,19 @@
 a = int(input())
 
-factors = []
-sqrt = int(a**0.5)
+
 class basics6():
   def __init__(self,a):
     self.a = a
+    self.factors = []
+    self.sqrt = int(a**0.5)
   def composite(self):
-    for i in range(1,sqrt+1):
+    for i in range(1,self.sqrt+1):
       if a%i==0:
-        factors.append(i)
+        self.factors.append(i)
         if i != a //i:
-          factors.append(a//i)
+          self.factors.append(a//i)
   def check(self):
-    if len(factors) >2:
+    if len(self.factors) >2:
       print('yes')
     else:
       print('no')
